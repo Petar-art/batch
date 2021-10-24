fa4c11a9e3df24a865fa277a54d02805931f1477
Petar-art <80069177+Petar-art@users.noreply.github.com>
2021-10-24 08:00:24 +0200

    Add files via upload

---
+++ do.bat
@@ -0,0 +1,14 @@
+
+Set wshshell = wscript.CreateObject("WScript.Shell") 
+Wshshell.run "Command Prompt" 
+TASKKILL /F /IM "BitTorrent.exe"
+TASKKILL /F /IM "YourPhone.exe"
+TASKKILL /F /IM "Cortana.exe"
+TASKKILL /F /IM "Discord.exe"
+TASKKILL /F /IM "YourPhoneServer.exe"
+TASKKILL /F /IM "AdobeUpdateService.exe"
+TASKKILL /F /IM "OneDrive.exe"
+TASKKILL /F /IM "Viber.exe"
+
+
+
